5c04d9ee4e44c19d467a47c75e5274ce36fad7ac
milindsardar <107666655+milindsardar@users.noreply.github.com>
2022-07-22 14:57:10 +0530

    adding reverse array program

---
+++ PrintreverseArray.java
@@ -0,0 +1,18 @@
+package array;
+
+public class PrintreverseArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr1[]= {1,2,3,4,5};
+int a=arr1.length;
+for(int b=0;b<=a-1;b++)
+{
+	System.out.print(" array in original way "+arr1[b]);
+	
+}
+
+}
+	}
+
+
